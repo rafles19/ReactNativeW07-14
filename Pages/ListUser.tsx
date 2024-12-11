eba413824e890d86f1857b177ebcacc493052972
rafleskristiyanto <sitiora19@gmail.com>
2024-12-11 17:26:34 +0700

    Week07-Reanimated

--- Pages/ListUser.tsx
+++ Pages/ListUser.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Image, ScrollView, Text, TouchableOpacity, View } from 'react-native';
+import Animated, { ZoomIn, BounceIn, FadeIn } from 'react-native-reanimated';
 import userData from './data.json';
-import styles from './Styles'; // Ensure styles are defined here
+import styles from './Styles';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { RouteProp } from '@react-navigation/native';
-
 
 type UserListNavigationProps = StackNavigationProp<RootStackParamList, 'UserList'>;
 type UserListRouteProps = RouteProp<RootStackParamList, 'UserList'>;
@@ -12,27 +12,35 @@ type Props = {
     navigation : UserListNavigationProps;
     route : UserListRouteProps;
 }
-
 const UserList = ({ navigation } : Props) => {
   return (
     <ScrollView>
-      {userData.map((user) => (
-        <View style={styles.userList} key={user.name}>
-          <TouchableOpacity
-            style={styles.card}
-            onPress={() => {
-                navigation.navigate('ProfileScreen', { username: user.name, photo_url: user.photo_url, email: user.email });
-              console.log("Navigating to Profile with user:", user.name);
-            }}
+      {userData.map((user, index) => {
+        const animation = index % 3 === 0
+          ? ZoomIn
+          : index % 3 === 1
+          ? BounceIn
+          : FadeIn;
+
+        return (
+          <Animated.View
+            key={user.name}
+            entering={animation.duration(5000 + index * 100)}
+            style={styles.userList}
           >
-            <Image style={styles.avatar} source={{ uri: user.photo_url }} />
-            <View>
-              <Text style={styles.boldText}>{user.name}</Text>
-              <Text>{user.email}</Text>
-            </View>
-          </TouchableOpacity>
-        </View>
-      ))}
+            <TouchableOpacity
+              style={styles.card}
+              onPress={() => navigation.navigate('ProfileScreen', { username: user.name, photo_url: user.photo_url, email: user.email })}
+            >
+              <Image style={styles.avatar} source={{ uri: user.photo_url }} />
+              <View>
+                <Text style={styles.boldText}>{user.name}</Text>
+                <Text>{user.email}</Text>
+              </View>
+            </TouchableOpacity>
+          </Animated.View>
+        );
+      })}
     </ScrollView>
   );
 };
